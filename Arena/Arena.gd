80338bedf7ffd71200bef80f0907a6dcbc818945
Matt Schwartz <7495188+n0spaces@users.noreply.github.com>
2020-10-17 10:03:48 -0500

    Add wheel spin button and gauge

--- Arena/Arena.gd
+++ Arena/Arena.gd
@@ -4,9 +4,18 @@ onready var enemy_scene := preload("res://Enemy/Enemy.tscn")
 
 
 func _ready():
-	spawn_enemy()
+	randomize()
+	start_wave(3)
+
+
+func start_wave(num_enemies = 3):
+	for i in range(3):
+		spawn_enemy()
 
 
 func spawn_enemy():
+	# spawn an enemy on a random point in the enemy spawn path
 	var enemy = enemy_scene.instance()
+	$EnemySpawnPath/PathFollow2D.unit_offset = randf()
+	enemy.position = $EnemySpawnPath/PathFollow2D.position
 	add_child(enemy)
---
+++ wheel/Gauge.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+var increasing := false
+var gauge_amount := 0.0  # 0 to 1
+var gauge_fill_speed := 1
+
+signal gauge_released(amount)
+
+
+func _ready():
+	clear()
+
+
+func _physics_process(delta):
+	if increasing:
+		gauge_amount = clamp(gauge_amount + gauge_fill_speed * delta, 0, 1)
+		# Move the second point in the line to increase the meter
+		$LineMeter.points[1].x = 39 * gauge_amount
+
+
+func clear():
+	gauge_amount = 0
+	$LineMeter.points[1].x = 0
+	$Button.disabled = false
+
+
+func _on_Button_button_down():
+	increasing = true
+
+
+func _on_Button_button_up():
+	$Button.disabled = true
+	increasing = false
+	emit_signal("gauge_released", gauge_amount)
