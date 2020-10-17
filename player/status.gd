1efaa765f5a3e00bb154b73700e20f0e4da873a9
Ethan Tucker <ectucker1@gmail.com>
2020-10-17 16:21:20 -0500

    Made the wheel spinning buttons look better

--- player/status.gd
+++ player/status.gd
@@ -6,7 +6,7 @@ extends Node
 var player
 
 # Define all variables needed to represent status effects
-var health := 50 setget set_health
+var health := 100 setget set_health
 
 var max_speed_mult := 1.0
 var acc_mult := 1.0
