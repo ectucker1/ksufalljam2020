03a6fa8aa5e1282f4b70c809d1b333d6d0d6e4ab
Matt Schwartz <7495188+n0spaces@users.noreply.github.com>
2020-10-16 22:59:56 -0500

    Add basic enemy melee attack

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -4,45 +4,68 @@ enum Type {  # placeholder enemy types
 	NONE, ZOMBIE, DOG, BIRD
 }
 
-export(Type) var type = Type.NONE
+var type = Type.NONE
 var speed := 50
+var melee_distance := 25
 
 var arena : Node2D
 var nav : Navigation2D
+var player : KinematicBody2D
 
 
 func _ready():
+	add_to_group("enemies")
 	arena = get_node("../../Arena")
 	if arena:
 		nav = arena.get_node("Nav")
 
 
 func _physics_process(delta):
+	player = get_tree().get_nodes_in_group("players")[0]
 	follow_player(delta)
 
 
 func follow_player(delta):
-	if !nav: return
+	# Skip if the level's navigation node doesn't exist
+	if !nav:
+		return
 	
-	var player = get_tree().get_nodes_in_group("players")[0]
-	
+	# Find a path to the player
 	var path = nav.get_simple_path(position, player.position)
 	
-	var collision : KinematicCollision2D
+	# Calculate this frame's walking distance
 	var distance_walk = speed * delta
 	
+	# Move until there's no more distance to walk
 	while distance_walk > 0 and path.size() > 0:
-		# determine distance to next point
+		# Determine distance to next point
 		var distance_point = position.distance_to(path[0])
 		
 		if distance_walk < distance_point:
-			collision = move_and_collide(position.direction_to(path[0]) * distance_walk)
+			move_and_collide(position.direction_to(path[0]) * distance_walk)
 		
 		else:
-			collision = move_and_collide(path[0] - position)
+			move_and_collide(path[0] - position)
 			path.remove(0)
 		
 		distance_walk -= distance_point
 	
-	if collision:
-		print(collision)
+	# Attack if close enough
+	if position.distance_to(player.position) < melee_distance:
+		melee_attack()
+
+
+func melee_attack():
+	# Don't attack if an animation is still playing (this serves as a cooldown)
+	if $MeleeArea/Anim.is_playing():
+		return
+	
+	# The attack animation enables the area's collision
+	$MeleeArea.look_at(player.position)
+	$MeleeArea/Anim.play("attack")
+
+
+func _on_MeleeArea_body_entered(body):
+	if body == player:
+		# TODO: deal damage
+		pass
