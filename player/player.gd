0d82006fa2fb090abc3f9d17bf1dc5e02af6f54d
Ethan Tucker <ectucker1@gmail.com>
2020-10-17 16:45:43 -0500

    Made the player hold still on the wheel screen

--- player/player.gd
+++ player/player.gd
@@ -20,6 +20,8 @@ var passives := []
 var velocity := Vector2.ZERO
 var velocity_override := Vector2.ZERO
 
+var enabled = false
+
 # Signals emitted when various events happen
 # These are neccessary for mutations to work
 signal hurt(amount)
@@ -39,74 +41,79 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# Call processing method on each mutation
-	if primary_active != null:
-		primary_active.process(delta)
-	if secondary_active != null:
-		secondary_active.process(delta)
-	for passive in passives:
-		passive.process(delta)
+	if not enabled and not get_parent().get_node("WheelLayer/Wheel").visible:
+		enabled = true
 	
-	$Sprites/RightSprite.visible = false
-	$Sprites/LeftSprite.visible = false
-	$Sprites/UpSprite.visible = false
-	$Sprites/DownSprite.visible = false
-	match get_facing():
-		GlobalConsts.DIR_RIGHT:
-			$Sprites/RightSprite.visible = true
-		GlobalConsts.DIR_LEFT:
-			$Sprites/LeftSprite.visible = true
-		GlobalConsts.DIR_UP:
-			$Sprites/UpSprite.visible = true
-		GlobalConsts.DIR_DOWN:
-			$Sprites/DownSprite.visible = true
+	if enabled:
+		# Call processing method on each mutation
+		if primary_active != null:
+			primary_active.process(delta)
+		if secondary_active != null:
+			secondary_active.process(delta)
+		for passive in passives:
+			passive.process(delta)
+		
+		$Sprites/RightSprite.visible = false
+		$Sprites/LeftSprite.visible = false
+		$Sprites/UpSprite.visible = false
+		$Sprites/DownSprite.visible = false
+		match get_facing():
+			GlobalConsts.DIR_RIGHT:
+				$Sprites/RightSprite.visible = true
+			GlobalConsts.DIR_LEFT:
+				$Sprites/LeftSprite.visible = true
+			GlobalConsts.DIR_UP:
+				$Sprites/UpSprite.visible = true
+			GlobalConsts.DIR_DOWN:
+				$Sprites/DownSprite.visible = true
 
 # Called at a fixed rate. 'delta' is the elapsed time since the previous call.
 func _physics_process(delta):
-	# Get the direction the player wants to move in
-	var input_dir = Vector2.ZERO
-	input_dir.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	input_dir.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	input_dir = input_dir.normalized()
-	
-	# Apply acceleration if we're holding a direction
-	if input_dir != Vector2.ZERO:
-		# Normal movement mode
-		if not status.tank_drive:
-			velocity += BASE_ACC * status.acc_mult * input_dir * delta
-		# Tank drive
+	if enabled:
+		# Get the direction the player wants to move in
+		var input_dir = Vector2.ZERO
+		input_dir.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+		input_dir.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+		input_dir = input_dir.normalized()
+		
+		# Apply acceleration if we're holding a direction
+		if input_dir != Vector2.ZERO:
+			# Normal movement mode
+			if not status.tank_drive:
+				velocity += BASE_ACC * status.acc_mult * input_dir * delta
+			# Tank drive
+			else:
+				$Treads.rotate(input_dir.x * TANK_ROT_SPEED)
+				velocity += Vector2(cos($Treads.rotation), sin($Treads.rotation)) * BASE_ACC * status.acc_mult * delta * -input_dir.y
+			# Clamp to max velocity
+			if velocity.length() > BASE_MAX_SPEED * status.max_speed_mult:
+					velocity = velocity.normalized() * BASE_MAX_SPEED * status.max_speed_mult
 		else:
-			$Treads.rotate(input_dir.x * TANK_ROT_SPEED)
-			velocity += Vector2(cos($Treads.rotation), sin($Treads.rotation)) * BASE_ACC * status.acc_mult * delta * -input_dir.y
-		# Clamp to max velocity
-		if velocity.length() > BASE_MAX_SPEED * status.max_speed_mult:
-				velocity = velocity.normalized() * BASE_MAX_SPEED * status.max_speed_mult
-	else:
-		velocity = velocity.normalized() * clamp(velocity.length() - STOPPING_FRICTION * delta, 0.0, INF)
-	
-	velocity_override = Vector2.ZERO
-	
-	# Call phyiscs processing method on each mutation
-	if primary_active != null:
-		primary_active.physics_process(delta)
-	if secondary_active != null:
-		secondary_active.physics_process(delta)
-	for passive in passives:
-		passive.physics_process(delta)
-	
-	if velocity_override != Vector2.ZERO:
-		velocity = velocity_override
-	
-	# Actually apply movement and update velocity if we hit something
-	velocity = move_and_slide(velocity, Vector2.ZERO)
-	
-	# If active keys are held, call their use
-	if Input.is_action_pressed("attack_primary"):
+			velocity = velocity.normalized() * clamp(velocity.length() - STOPPING_FRICTION * delta, 0.0, INF)
+		
+		velocity_override = Vector2.ZERO
+		
+		# Call phyiscs processing method on each mutation
 		if primary_active != null:
-			primary_active.on_used()
-	if Input.is_action_pressed("attack_secondary"):
+			primary_active.physics_process(delta)
 		if secondary_active != null:
-			secondary_active.on_used()
+			secondary_active.physics_process(delta)
+		for passive in passives:
+			passive.physics_process(delta)
+		
+		if velocity_override != Vector2.ZERO:
+			velocity = velocity_override
+		
+		# Actually apply movement and update velocity if we hit something
+		velocity = move_and_slide(velocity, Vector2.ZERO)
+		
+		# If active keys are held, call their use
+		if Input.is_action_pressed("attack_primary"):
+			if primary_active != null:
+				primary_active.on_used()
+		if Input.is_action_pressed("attack_secondary"):
+			if secondary_active != null:
+				secondary_active.on_used()
 
 func get_facing():
 	var input_dir = Vector2.ZERO
