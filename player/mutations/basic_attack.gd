a3a3bbd4d6541b4660a46acc6a355cdf228398c6
Ethan Tucker <ectucker1@gmail.com>
2020-10-17 00:01:33 -0500

    Made the player attack mouse aimed

--- player/mutations/basic_attack.gd
+++ player/mutations/basic_attack.gd
@@ -16,14 +16,5 @@ func physics_process(delta):
 func on_used():
 	if timeout <= 0.0:
 		timeout = COOLDOWN
-		var animation_choice = ""
-		match player.get_facing():
-			GlobalConsts.DIR_UP:
-				animation_choice = "AttackUp"
-			GlobalConsts.DIR_DOWN:
-				animation_choice = "AttackDown"
-			GlobalConsts.DIR_LEFT:
-				animation_choice = "AttackLeft"
-			GlobalConsts.DIR_RIGHT:
-				animation_choice = "AttackRight"
-		player.get_node("Attacks/BasicAttack/AnimationPlayer").play(animation_choice)
+		player.get_node("Attacks/BasicAttack/HitBox").look_at(player.get_global_mouse_position())
+		player.get_node("Attacks/BasicAttack/AnimationPlayer").play("Attack")
