1a36676cd587a3fc4f247a762c115504b2a1f519
Matt Schwartz <7495188+n0spaces@users.noreply.github.com>
2020-10-17 23:22:39 -0500

    Update ground

--- Arena/tilemaps/tileset.tsx
+++ Arena/tilemaps/tileset.tsx
@@ -1,49 +1,58 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="set" tilewidth="16" tileheight="16" tilecount="15" columns="0">
+<tileset version="1.4" tiledversion="1.4.2" name="set" tilewidth="16" tileheight="16" tilecount="18" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Pillar_base.png"/>
+  <image width="16" height="16" source="../tiles/Pillar_base.png"/>
  </tile>
  <tile id="1">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Pillar_top.png"/>
+  <image width="16" height="16" source="../tiles/Pillar_top.png"/>
  </tile>
  <tile id="2">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Sand_plain.png"/>
+  <image width="16" height="16" source="../tiles/Sand_plain.png"/>
  </tile>
  <tile id="3">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Sand_rl.png"/>
+  <image width="16" height="16" source="../tiles/Sand_rl.png"/>
  </tile>
  <tile id="4">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Spike_trap.png"/>
+  <image width="16" height="16" source="../tiles/Spike_trap.png"/>
  </tile>
  <tile id="5">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Wall_bl.png"/>
+  <image width="16" height="16" source="../tiles/Wall_bl.png"/>
  </tile>
  <tile id="6">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Wall_br.png"/>
+  <image width="16" height="16" source="../tiles/Wall_br.png"/>
  </tile>
  <tile id="7">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Wall_lr.png"/>
+  <image width="16" height="16" source="../tiles/Wall_lr.png"/>
  </tile>
  <tile id="8">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Wall_lrb.png"/>
+  <image width="16" height="16" source="../tiles/Wall_lrb.png"/>
  </tile>
  <tile id="9">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Wall_lrt.png"/>
+  <image width="16" height="16" source="../tiles/Wall_lrt.png"/>
  </tile>
  <tile id="10">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Wall_tbc.png"/>
+  <image width="16" height="16" source="../tiles/Wall_tbc.png"/>
  </tile>
  <tile id="11">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Wall_tlb.png"/>
+  <image width="16" height="16" source="../tiles/Wall_tlb.png"/>
  </tile>
  <tile id="12">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Wall_tlt.png"/>
+  <image width="16" height="16" source="../tiles/Wall_tlt.png"/>
  </tile>
  <tile id="13">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Wall_trb.png"/>
+  <image width="16" height="16" source="../tiles/Wall_trb.png"/>
  </tile>
  <tile id="14">
-  <image width="16" height="16" source="GitHub/ksufalljam2020/Arena/tiles/Wall_trt.png"/>
+  <image width="16" height="16" source="../tiles/Wall_trt.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../tiles/Pillar_ground.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="../tiles/sand_grassy.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="../tiles/sand_rocks.png"/>
  </tile>
 </tileset>
